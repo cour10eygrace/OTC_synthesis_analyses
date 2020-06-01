e781fa87a22cb11cae715635449cd6609b074f87
Courtney Collins <courtneygrace125@gmail.com>
2020-06-01 15:48:22 -0600

    Add files via upload

---
+++ Scripts/OTC_int_cens_reg_disp.R
@@ -0,0 +1,122 @@
+##load and organize data----
+load("data/Courtney/OTC_analysis/fruitdata.RData")
+subsites<-read.csv("data/LOOKUPS/subsites.csv") 
+
+library(brms)
+library(dplyr)#make sure plyr is not on
+library(tidyr)
+library(survival)
+
+####scale responses
+disp=disp%>%
+  mutate(midpoint=(doy+prior_visit)/2)
+
+mn=mean(disp$midpoint, na.rm=T)
+sd=sd(disp$midpoint, na.rm=T)
+
+disp=disp%>%
+  mutate(prior_visit = (prior_visit-mn)/sd)%>%
+  mutate(doy = (doy-mn)/sd)
+
+#subset to subsite-years with both CTL and OTC 
+disp<-unite(disp, all, spp, subsite, year, remove=F)
+check<-group_by(disp, spp, subsite, year)%>%summarise(n = n_distinct(treatment))%>%filter(n>1)%>%
+  unite(all, spp, subsite, year)
+dispcheck<-filter(disp, all %in% check$all)
+
+#subset to replicates with 2 or more observations  
+dispcheck2<-group_by(dispcheck, year, spp, site_name, subsite, treatment)%>% count()%>% filter(n>1)%>%
+  unite(all, year, spp, site_name, subsite, treatment)
+dispcheck<-unite(dispcheck,all, year, spp, site_name, subsite, treatment, remove = FALSE)
+dispcheck<-filter(dispcheck, all %in% dispcheck2$all)
+
+# -- RUN CENSORED REGRESSION ----
+df=dispcheck
+dispcheck$treatment<-factor(dispcheck$treatment)
+
+regmods=df%>%group_by(
+  year, spp, site_name, subsite, treatment,
+)%>%
+  do(mod = try(survreg(Surv(prior_visit, doy, type = "interval2") ~ 1,
+                       data = ., dist = "gaussian"
+  )%>%broom::tidy(.))
+  )
+
+#unnest model column
+regmods=regmods%>%  unnest(., cols=mod)
+
+##pull out intercept estimates, fill in NAN estimates/std.errors from individuals with identical doys 
+#use midpoint between doy, prior_visit for estimate 
+#use avg std.error for each spp x subsite across other years for std. error 
+regmodsx<-subset(regmods, term=='(Intercept)')
+regmodsy<-subset(regmodsx, estimate=="NaN")
+regmodsy<-unite(regmodsy,all, year, spp, site_name, subsite, treatment, remove = FALSE)
+dispcheck2<-filter(dispcheck, all %in% regmodsy$all)%>%select(all, doy, prior_visit)%>%distinct(.)
+regmodsy<-left_join(regmodsy, dispcheck2, by='all')%>%mutate(., estimate=(doy+prior_visit)/2)
+regmodsx<-subset(regmodsx, estimate!="NaN")
+spp_se<-group_by(regmodsx, spp, subsite, treatment)%>%summarise(se=mean(std.error))
+regmodsz<-left_join(regmodsy, spp_se)%>%mutate(std.error=se)
+
+regmodsx<-as.data.frame(rbind(regmodsx, select(regmodsz, -doy, -prior_visit, -all, -se)))%>% 
+  filter(std.error>0)
+
+#require that every replicate has paired OTC and CTL
+regmodsxx<-group_by(regmodsx, spp, subsite, year)%>%
+  summarise(n_treat = n_distinct(treatment))%>%filter(n_treat>1)
+regmodsx<-left_join(regmodsx, regmodsxx)%>%filter(!is.na(n_treat))
+
+#add in spatiotemporal info
+ecosys<-dplyr::select(subsites, site_name,subsite, OTCWinterRemoval, exstart, lat, commtype, Ecosystem)%>%distinct(.)
+regmodsx<-left_join(regmodsx, ecosys)
+regmodsx$years_warm<-regmodsx$year-as.numeric(as.character(regmodsx$exstart))
+regmodsx<-mutate(regmodsx, years_warm=ifelse(years_warm<1,1, years_warm)) #lowest value for years_warm is 1 
+
+#scale continuous predictors 
+mn=mean(regmodsx$years_warm)
+sd=sd(regmodsx$years_warm)
+mn2=mean(regmodsx$lat)
+sd2=sd(regmodsx$lat)
+
+regmodsx=regmodsx%>%
+  mutate(years_warm= (years_warm-mn)/sd)%>%
+  mutate(lat = (lat-mn2)/sd2)
+
+#brms model setup----
+m2x<-bf(estimate|resp_se(std.error, sigma = TRUE)~ treatment + (treatment|site_name)+ (treatment|site_name:year) + (treatment|spp)+ (treatment|site_name:subsite)) 
+fit_m2x_disp<- brm(m2x, data = regmodsx, control = list(adapt_delta=0.99, max_treedepth = 15), cores=2, chains=2, iter=10000, family=gaussian)
+save(fit_m2x_disp, file="fit_m2x_disp.Rdata")
+
+m3x<-bf(estimate|resp_se(std.error, sigma = TRUE)~ treatment*years_warm + (treatment|site_name)+ (treatment|site_name:year) + (treatment|spp)+ (treatment|site_name:subsite)) 
+fit_m3x_disp<- brm(m3x, data = regmodsx, control = list(adapt_delta=0.99, max_treedepth = 15), cores=2, chains=2, iter=10000, family=gaussian)
+save(fit_m3x_disp, file="fit_m3x_disp.Rdata")
+
+m4x<-bf(estimate|resp_se(std.error, sigma = TRUE)~ treatment*lat + (treatment|site_name)+ (treatment|site_name:year) + (treatment|spp)+ (treatment|site_name:subsite)) 
+fit_m4x_disp<- brm(m4x, data = regmodsx, control = list(adapt_delta=0.99, max_treedepth = 15), cores=2, chains=2, iter=10000, family=gaussian)
+save(fit_m4x_disp, file="fit_m4x_disp.Rdata")
+
+m5x<-bf(estimate|resp_se(std.error, sigma = TRUE)~ treatment*commtype + (treatment|site_name)+ (treatment|site_name:year) + (treatment|spp)+ (treatment|site_name:subsite)) 
+fit_m5x_disp<- brm(m5x, data = regmodsx, control = list(adapt_delta=0.99, max_treedepth = 15), cores=2, chains=2, iter=10000, family=gaussian)
+save(fit_m5x_disp, file="fit_m5x_disp.Rdata")
+
+m6x<-bf(estimate|resp_se(std.error, sigma = TRUE)~ treatment*OTCWinterRemoval + (treatment|site_name)+ (treatment|site_name:year) + (treatment|spp)+ (treatment|site_name:subsite)) 
+fit_m6x_disp<- brm(m6x, data = regmodsx, control = list(adapt_delta=0.99, max_treedepth = 15), cores=2, chains=2, iter=10000, family=gaussian)
+save(fit_m6x_disp, file="fit_m6x_disp.Rdata")
+
+#add in climate info 
+load("data/Courtney/OTC_analysis/climate_phenology.Rdata")
+regmodsx<-left_join(regmodsx, avg_clim_disp)
+regmodsx<-filter(regmodsx, !is.na(siteT)) #NAs mean that more than 5 days of the prior-period were infilled 
+
+#scale continuous predictors 
+mn3=mean(regmodsx$siteT)
+sd3=sd(regmodsx$siteT)
+mn4=mean(regmodsx$siteyear_deltaT)
+sd4=sd(regmodsx$siteyear_deltaT)
+
+regmodsx=regmodsx%>%
+  mutate(siteT = (siteT-mn3)/sd3)%>%
+  mutate(siteyear_deltaT = (siteyear_deltaT-mn4)/sd4)
+
+m7x<-bf(estimate|resp_se(std.error, sigma = TRUE)~ treatment*siteT + treatment*siteyear_deltaT + (treatment|site_name)+ (treatment|site_name:year) + (treatment|spp)+ (treatment|site_name:subsite)) 
+fit_m7x_disp<- brm(m7x, data = regmodsx, control = list(adapt_delta=0.99, max_treedepth = 15), cores=2, chains=2, iter=10000, family=gaussian)
+save(fit_m7x_disp, file="fit_m7x_disp.Rdata")
