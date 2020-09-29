92ac4306636dc4295695a8aaa5c3fa40e17e361b
cour10eygrace <courtneygrace125@gmail.com>
2020-09-29 10:43:07 -0600

    Jakob updates to map

--- Figures/map_fig.R
+++ Figures/map_fig.R
@@ -5,6 +5,12 @@ library(raster)
 library(maptools)
 library(dplyr)
 library(RColorBrewer)
+library(sf) # Simple feature geometries
+library(rnaturalearth) # National boundaries from https://www.naturalearthdata.com/
+library(rnaturalearthdata) 
+library(cowplot) # Added simplicity for ggplot themes and export
+library(ggrepel) # Avoid overlap when placing ggplot labels
+
 
 # organize Site coordinates
 subsites<-read.csv("data/LOOKUPS/subsites.csv") 
@@ -16,64 +22,29 @@ sites<-dplyr::select(sites, site_name, lat,long, subsite)%>%group_by(site_name)%
   mutate(subsite_no=n_distinct(subsite))%>%mutate(lat=mean(lat, na.rm=T), long=mean(long, na.rm=T))%>%
   dplyr::select(-subsite)%>%distinct(.)%>%arrange(site_name, long, lat, subsite_no)
 
-sites_map <- SpatialPointsDataFrame(coords = sites[,c(3,2)], data=sites[,c(1,4)], proj4string = CRS("+proj=longlat +datum=WGS84 +no_defs"))
+sites$site_name_long <- sort(c(
+  "Alexandra Fiord", 
+  "Endalen", 
+  "Adventdalen",   
+  "Barrow-Utqiagvik",
+  "Atqasuk", 
+  "Toolik Lake", 
+  "Imnavait Creek", 
+  "Latnjajaure", 
+  "Kangerlussuaq", 
+  "Daring Lake", 
+  "Healy", 
+  "Faroe Islands", 
+  "Finse", 
+  "Jakobshorn", 
+  "Val Bercla", 
+  "Gavia Pass", 
+  "Niwot Ridge", 
+  "White Mountains"))
 
-# Create world map using maps
-wrld <- map(plot=FALSE, interior=FALSE, wrap=TRUE, ylim=c(35, 80), xlim=c(-180, 30))
-# Transform map to SpatialLines object
-wrld_sp <- map2SpatialLines(wrld)
-# Set CRS to lat lon (conversion does not carry it across)
-proj4string(wrld_sp) <- CRS("+proj=longlat")
+sites$site_name_long[sites$site_name == "BARROW"] <- "Utqiagvik"
 
-# Project map and sites into Canadian Centred polar projection.
-#laea_wrld_sp <- spTransform(wrld_sp, CRS("+proj=aea +lat_0=90 +lon_0=-100 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"))
-#laea_sites <- spTransform(sites_map,CRS("+proj=laea +lat_0=90 +lon_0=-100 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs") )
-
-# Plot map
-plot(wrld_sp, col = 'black', lwd = 0.3, cex= 1)
-# Plot Sites onto map
-plot(sites_map, cex = 1, pch=21,
-     col = "black",  
-     #red and blue pallettes for siteT-not in order 
-     #bg=c("#FFFFCC","#FFEDA0","#FED976","#FEB24C","#FD8D3C","#FC4E2A","#E31A1C","#BD0026","#800026", 
-     #     "#F7FBFF" ,"#DEEBF7" ,"#C6DBEF" ,"#9ECAE1" ,"#6BAED6" ,"#4292C6" ,"#2171B5" ,"#08519C" ,"#08306B"),
-     bg=c("#1B9E77", "#D95F02" ,"#7570B3" ,"#E7298A" ,"#66A61E", "#E6AB02" ,"#A6761D" ,"#666666","#4575B4"),
-          #"#324D5CFF", 
-          # "#46B29DFF", 
-          # "#C2A33EFF",
-          # "#E37B40FF"),
-     add = T, color=sites_map$siteT)
-# Add labels
-text(sites_map, c("ADVENTDALEN", "ALEXFIORD",
-                   "ATQASUK", "UTQUIAVIK",
-                   "DARING", "ENDALEN",
-                   "FAROE", "FINSE" ,"GAVIAPASS", 
-        "HEALY", "IMNAVAIT",  "JAKOBSHORN", "KANGER",
-        "LATNJA", "NIWOT", "TOOLIK" , "VALBERCLA", "WHITEMTNS"),
-                    font = 2, cex = 0.75,
-     col = c("#1B9E77", "#D95F02" ,"#7570B3" ,"#E7298A" ,"#66A61E", "#E6AB02" ,"#A6761D" ,"#666666","#4575B4"),
-      #c("#324D5CFF", 
-            # "#46B29DFF", 
-            # "#C2A33EFF",
-            # "#E37B40FF",),
-     pos = c(1,4,4,2,1,2,1,4,4,4,4,3,4,4,1,2,2,1), offset = 0.5, halo = F)
-
-
-## From Jakob: Suggestions for maps added 15 September 2020 ----
-# Here a simple example using simple feautres (sf) and boundaries from
-# natural earth. Simple features easily integrate into ggplot.
-# I have left some comments, but it's hopefully pretty self explanatory.
-
-# Additional dependencies
-library(sf) # Simple feature geometries
-
-library(rnaturalearth) # National boundaries from https://www.naturalearthdata.com/
-library(rnaturalearthdata) 
-
-library(cowplot) # Added simplicity for ggplot themes and export
-library(ggrepel) # Avoid overlap when placing ggplot labels
-
-# Specify a lamberd conformal conic projection, which works nicely
+# Specify a lambert conformal conical projection, which works nicely
 # for the area covered by the sites.
 # I use a WKT specification here as this is most easy to understand
 # You can change the "Central_Meridian" parameter to shift the central
@@ -96,35 +67,13 @@ lambert_conformic <- 'PROJCS["North_America_Lambert_Conformal_Conic",
                             UNIT["Meter",1],
                             AUTHORITY["EPSG","102009"]]'
 
-# Generate politcial boundaries as sf and transform from lat long to lambert
+# Generate political boundaries as sf and transform from lat long to lambert
 world <- ne_countries(scale = "medium", returnclass = "sf")
 world_lambert <- st_transform(world, crs = st_crs(lambert_conformic))
 
 # Generate sf object for site point sources and transform
 sites_sf <- st_as_sf(sites, coords = c("long", "lat"), crs = 4326)
 sites_sf <- st_transform(sites_sf , crs = st_crs(lambert_conformic))
-#fix names
-sites_sf<-mutate(sites_sf, site_name2=case_when(site_name=="ADVENTDALEN"~"ADVENTDALEN", 
-                                               site_name=="ALEXFIORD"~"ALEXANDRA FIORD", 
-                                               site_name=="ATQASUK"~"ATQASUK",
-                                               site_name=="BARROW"~"UTQIAGVIK",
-                                               site_name=="DARING"~"DARING LAKE",
-                                               site_name=="ENDALEN"~"ENDALEN",
-                                               site_name=="FAROE"~"FAROE ISLANDS",
-                                               site_name=="FINSE"~"FINSE",
-                                               site_name=="GAVIAPASS"~"GAVIA PASS",
-                                               site_name=="HEALY"~"HEALY",
-                                               site_name=="TOOLIK"~"TOOLIK LAKE",
-                                               site_name=="IMNAVAIT"~"IMNAVAIT CREEK",
-                                               site_name=="JAKOBSHORN"~"JAKOBSHORN",
-                                               site_name=="KANGER"~"KANGERLUSSUAQ",
-                                               site_name=="KANGER"~"KANGERLUSSUAQ",
-                                               site_name=="LATNJA"~"LATNJAJAUARE",
-                                               site_name=="NIWOT"~"NIWOT RIDGE",
-                                               site_name=="VALBERCLA"~"VAL BERCLA",
-                                               site_name=="WHITEMTNS"~"WHITE MTNS"))
-                                               
-                  
 
 # Next we need to define the area we want to plot
 # For this we get the minimum and maximum coordinates and 
@@ -141,15 +90,16 @@ map_extent <- sites_sf %>% st_coordinates() %>%
 otc_site_map <- ggplot() + 
   # geom_sf to plot the country boundaries
   geom_sf(data = world_lambert,
-          fill = "#d3d3d366", 
+          fill = "#d3d3d3FF", 
+          colour = "white",
           size = 0.25) + 
   # geom_sf to plot the sites (it automatically knows they're points)
-  geom_sf(data = sites_sf, colour = "red",
-          fill = "#ffffff00", shape = 21, size = 2,
-          stroke = 2) +  
+  geom_sf(data = sites_sf, colour = "#498CB8FF",
+          fill = "#ffffff00", shape = 21, size = 1.5,
+          stroke = 1.5) +  
   # geom_label_repel with st_coordinates as stats to plot labels 
   # that are not overlapping
-  geom_label_repel(data = sites_sf, aes(label = site_name2,
+  geom_label_repel(data = sites_sf, aes(label = site_name_long,
                                         geometry = geometry),
                    stat = "sf_coordinates",
                    size = 3,
@@ -170,21 +120,15 @@ otc_site_map <- ggplot() +
         axis.line = element_blank(),
         axis.ticks = element_blank(),
         axis.ticks.length = unit(0, "mm"),
+        axis.title = element_blank(),
         panel.grid.minor = element_line(colour = "white"), # Just in case you would like to modify these
         panel.grid.major = element_line(colour = "white"), # Just in case you would like to modify these
         plot.margin=grid::unit(c(0,0,0,0), "mm"),
-        panel.background = element_rect(fill = "white") # Just in case you would like to modify these
-        ) 
+        panel.background = element_rect(fill = "white"), # Just in case you would like to modify these
+        panel.border = element_rect(colour = "lightgrey", fill=NA, size=0.5)
+  ) 
 
 # A quick plot on the native graphics device
 otc_site_map
 
-# Export as file using save_plot and the aspect ratio deterimined from the
-# min and max coordinates.
-save_plot(otc_site_map, filename = "plots/maps/OTC_map_ver2.png",
-          base_aspect_ratio = 
-            (map_extent$xmax - map_extent$xmin) /
-            (map_extent$ymax - map_extent$ymin),
-          #base_height = 4 # Just in case you want to modify the base height
-          ) 
 
