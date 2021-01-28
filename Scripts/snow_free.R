9bd811a713994bed25b8b15ceb1850444cc46568
cour10eygrace <courtneygrace125@gmail.com>
2021-01-28 12:52:47 -0700

    snowfree data and model for OTC ms

--- Scripts/snow_free.R
+++ Scripts/snow_free.R
@@ -1,8 +1,12 @@
 library(brms)
 #read in available snowmelt data 
 sf<-read.csv("Data/Climate.data/snowfree_data.csv")
-
 #test effects of OTC and OTC period on doy snowfree 
-#OTC winter removal Y=Summer only, OTC winter removal N=Year round 
-fit_snowfree<-brm(doy~treatment* OTCWinterRemoval +(1|site_name) + (1|site_name:year)+
-        (1|site_name:subsite) , data=sf, control = list(adapt_delta=0.99, max_treedepth = 15), cores=2, chains=2, iter=2000, family=gaussian)
+
+#subset to sites with only year round bc OTCs do not affect snowmelt in summer only sites!! 
+sf2<-filter(sf, OTCWinterRemoval=="N")
+
+fit_snowfree<-brm(doy~treatment +(1|site_name) + (1|site_name:year)+
+            (1|site_name:subsite) , data=sf2, control = list(adapt_delta=0.99, max_treedepth = 15), cores=2, chains=2, iter=2000, family=gaussian)
+summary(fit_snowfree2)
+
